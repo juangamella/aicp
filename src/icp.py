b9e14bc2cf306857377d04a9c9d93294cc87e56c
Juan Gamella <juangamella@gmail.com>
2019-10-26 13:10:35 +0200

    First iteration of the ICP algorithm, using t-test and f-test on residuals

--- src/icp.py
+++ src/icp.py
@@ -7,24 +7,92 @@ from sklearn.linear_model import LinearRegression
 
 from functools import reduce
 import itertools
-# class ICP():
-#     """
-#     Contains the implementation of the ICP algorithm.
-#     Parameters:
 
-#     Methods:
-#       - fit: perform ICP on a given dataset
-#     """
-    
-#     def __init__(debug=False):
-#         return None
+#---------------------------------------------------------------------
+# "Public" API: icp function
 
-#     def fit(environments, target, alpha, method='residuals', debug=False):
+def icp(environments, target, alpha=0.05, max_predictors=None, debug=False):
+    """
+    ICP on the given target using data from the given environments
+    """
+    data = Data(environments, target)
+    # Test for causal predictor sets of increasing size
+    max_predictors = data.p-1 if max_predictors is None else max_predictors
+    base = set(range(data.p))
+    base.remove(target)
+    S = base.copy()
+    max_size = 0
+    while max_size <= max_predictors and len(S) > 0:
+        print("Evaluating candidate sets with length %d" % max_size) if debug else None
+        candidates = itertools.permutations(base, max_size)
+        for s in candidates:
+            # Find linear coefficients on pooled data
+            beta = regress(s, data)
+            assert((beta[list(base.difference(s))] == 0).all())
+            p_value = test_hypothesis(beta, data, debug=debug)
+            if p_value >= alpha:
+                S = S.intersection(s)
+                beta_str = np.array_str(beta, precision=2)
+                print("%s - %s (p=%0.2f) - S = %s %s" % (s, True, p_value, S, beta_str)) if debug else None
+            else:
+                print("%s - %s (p=%0.2f) - S = %s" % (s, False, p_value, S)) if debug else None
+            if len(S) == 0:
+                    break;
+        max_size += 1
+    return S
 
-        
-#         if method=='residuals':
-            
-        
+# Support functions to icp
+
+def test_hypothesis(beta, data, debug=False):
+    """Test hypothesis for a vector of coefficients beta, using the t-test for the mean
+    and f-test for the variances, and returning the p-value
+
+    """
+    mean_pvalues = np.zeros(data.n_env)
+    var_pvalues = np.zeros(data.n_env)
+    for i in range(data.n_env):
+        (env_targets, env_data, others_targets, others_data) = data.split(i)
+        residuals_env = env_targets - env_data @ beta
+        residuals_others = others_targets - others_data @ beta
+        mean_pvalues[i] = t_test(residuals_env, residuals_others)
+        var_pvalues[i] = f_test(residuals_env, residuals_others)
+        assert(mean_pvalues[i] <= 1)
+        assert(var_pvalues[i] <= 1)
+    # Combine via bonferroni correction
+    pvalue_mean = np.mean(mean_pvalues)
+    pvalue_var = np.mean(var_pvalues)
+    # Return two times the smallest p-value
+    return min(pvalue_mean, pvalue_var) * 2
+
+def regress(s, data, pooling=True, debug=False):
+    """
+    Perform linear regression of data.target over the variables indexed by s
+    """
+    supp = list(s) + [data.p]
+    if pooling:
+        X = data.pooled_data()[:,supp]
+        Y = data.pooled_targets()
+    beta = np.zeros(data.p+1)
+    beta[supp] = LinearRegression(fit_intercept=False).fit(X, Y).coef_
+    return beta
+
+def t_test(X,Y):
+    """Return the test statistic and p-value of the two sample f-test for
+    the given sample"""
+    result = ttest(X, Y, equal_var=False)
+    return result.pvalue
+
+def f_test(X,Y):
+    """Return the test statistic and p-value of the two sample g-test for
+    the given sample"""
+    X = X[np.isfinite(X)]
+    Y = Y[np.isfinite(Y)]
+    F = np.var(X, ddof=1) / np.var(Y, ddof=1)
+    p = f.cdf(F, len(X)-1, len(Y)-1)
+    return  2*min(p, 1-p)
+
+#---------------------------------------------------------------------
+# Data class and its support functions
 
 class Data():
     """Class to handle access to the dataset. Takes a list of
@@ -35,6 +103,7 @@ class Data():
       - p: the number of variables
       - n: the total number of samples
       - N: list with number of samples in each environment
+      - n_env: the number of environments
       - targets: list with the observations of the target in each environment
       - data: list with the observations of the other vars. in each environment
       - target: the index of the target variable
@@ -55,9 +124,10 @@ class Data():
         self.N = np.array(list(map(len, environments)))
         self.p = environments[0].shape[1]
         self.n = np.sum(self.N)
-        # Split environments into targets and remaining variables (adding a col. of 1s for the intercept)
+        self.n_env = len(environments)
+        # Extract targets and add a col. of 1s for the intercept
         self.targets = list(map(lambda e: e[:,target], environments))
-        self.data = list(map(lambda e: np.hstack([np.ones((len(e),1)), e[:, np.arange(self.p) != target]]), environments))
+        self.data = list(map(lambda e: np.hstack([e, np.ones((len(e),1))]), environments))
         self.target = target
 
     def pooled_data(self):
@@ -81,63 +151,13 @@ def pool(arrays, axis):
     """Takes a list() of numpy arrays and returns them in an new
     array, stacked along the given axis.
     """
-    stack_fun = np.vstack if axis==0 else np.hstack
-    return reduce(lambda acc, array: stack_fun([acc, array]), arrays)
-    
-def test_hypothesis(s, data, debug=False):
-    return np.random.uniform()
-#     E = len(environments)
-#     p_value_mean = np.zeros(E)
-#     p_value_variance = np.zeros_like(E)
-#     last = 0
-#     for i, e in enumerate(environments):
-#         e_data = e
-#         others_data = reduce(lambda acc, environments[np.arange(E) != i]
-        
-#         res_e = pooled_targets(
-        
-            
-from sampling import LGSEM
+    if len(arrays) == 1:
+        return arrays[0]
+    else:
+        stack_fun = np.vstack if axis==0 else np.hstack
+        return reduce(lambda acc, array: stack_fun([acc, array]), arrays)
 
-sem = LGSEM(10,1,1,1,1)
-e1 = sem.sample(4)
-e2 = sem.sample(3, do_interventions = np.array([[0,2]]))
-e3 = sem.sample(2, noise_interventions = np.array([[3,2,1]]))
-
-environments = [e1, e2, e3]
-target = 2
-debug=True
-alpha=0.05
-max_predictors = None
-
-###############################################
-
-
-data = Data(environments, target)
-if max_predictors is None:
-    max_predictors = data.p
-
-# Find linear coefficients on pooled data
-beta = LinearRegression(fit_intercept=False).fit(data.pooled_data(), data.pooled_targets()).coef_
-
-# Test for causal predictor sets of increasing size
-base = set(range(data.p))
-base.remove(target)
-S = base.copy()
-max_size = 0
-while max_size <= max_predictors and len(S) > 0:
-    print("Evaluating candidate sets with length %d" % max_size) if debug else None
-    candidates = itertools.permutations(base, max_size)
-    for s in candidates:
-        p_value = test_hypothesis(s, data, debug=debug)
-        holds = p_value <= alpha
-        S = S.intersection(s) if holds else S
-        print("%s - %s (p=%0.2f) - S = %s" % (s, holds, p_value, S)) if debug else None
-        if len(S) == 0:
-            break;
-    max_size += 1        
-
-
+#---------------------------------------------------------------------
 # Unit testing
 
 import unittest
@@ -162,6 +182,7 @@ class DataTests(unittest.TestCase):
         self.assertTrue((data.N == self.N).all())
         self.assertEqual(data.p, self.p)
         self.assertEqual(data.target, self.target)
+        self.assertEqual(data.n_env, len(self.environments))
         
     def test_targets(self):
         data = Data(self.environments, self.target)
@@ -176,8 +197,9 @@ class DataTests(unittest.TestCase):
         data = Data(self.environments, self.target)
         truth_pooled = []
         for i,ne in enumerate(self.N):
-            sample = np.ones(self.p)
-            sample[1:] *= (i+1)
+            sample = np.ones(self.p+1)
+            sample[:-1] *= (i+1)
+            sample[self.target] *= -1
             truth = np.tile(sample, (ne, 1))
             self.assertTrue((truth == data.data[i]).all())
             truth_pooled = truth if i==0 else np.vstack([truth_pooled, truth])
@@ -199,7 +221,6 @@ class DataTests(unittest.TestCase):
             # Test that rt and rd are correct
             idx = np.arange(self.n)
             idx = np.logical_or(idx < last, idx >= last+ne)
-            print(idx)
             truth_rt = data.pooled_targets()[idx]
             truth_rd = data.pooled_data()[idx, :]
             self.assertTrue((truth_rt == rt).all())
