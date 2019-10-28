4de3ef5ee542cf6615c864d3a17d37fbf831ce8a
Juan Gamella <juangamella@gmail.com>
2019-10-28 16:45:58 +0100

    Ongoing

--- src/main.py
+++ src/main.py
@@ -1,26 +1,30 @@
 os.chdir('/home/juan/ETH/code_semester_project/src')
 import networkx as nx
 import matplotlib.pyplot as plt
-def plot_graph(W):
-    G = nx.from_numpy_matrix(W, create_using = nx.DiGraph)
-    pos = nx.spring_layout(G)
+def plot_graph(sem):
+    G = nx.from_numpy_matrix(sem.W, create_using = nx.DiGraph)
+    pos = {}
+    for i,node in enumerate(sem.ordering):
+        x = (-1.1)**i
+        y = -i*0.5
+        pos[node] = (x,y)
     nx.draw(G, pos, with_labels=True)
-    print("a: %s" % pos)
     labels = nx.get_edge_attributes(G,'weight')
     nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-    plt.show()
+    plt.show(block = False)
 
 from sampling import LGSEM
 from icp import icp
+import numpy as np
 
-W = np.array([[0, 1, 1, 1, 0],
+W = np.array([[0, 1, -1, 0, 0],
               [0, 0, 0, 1, 0],
-              [0, 0, 0, 0, 1],
+              [0, 0, 0, 1, 0],
               [0, 0, 0, 0, 1],
               [0, 0, 0, 0, 0]])
 p = len(W)
 ordering = np.arange(p)
-sem = LGSEM(W,ordering,(0.5,0.5))
+sem = LGSEM(W,ordering,(1,1))
 n = round(1e5)
 e = sem.sample(n)
 e0 = sem.sample(n, noise_interventions = np.array([[0,5,1]]))
@@ -28,11 +32,16 @@ e1 = sem.sample(n, noise_interventions = np.array([[1,5,1]]))
 e2 = sem.sample(n, do_interventions = np.array([[2,5,1]]))
 e4 = sem.sample(n, do_interventions = np.array([[4,5,1]]))
 print(sem.W)
+#_ = plot_graph(sem)
 
-#plot_graph(sem.W)
-
-environments = [e, e0]#, e2, e3, e4
+environments = [e, e1, e4]#, e2, e3, e4
 target = 3
 
-S = icp(environments, target, alpha=0.05, max_predictors=None, debug=True)
-print("S = %s" % S)
+(S, accepted) = icp(environments, target, alpha=0.01, max_predictors=None, debug=True, stop_early=False)
+S,accepted
+
+from icp import Data
+from icp import confidence_intervals
+
+data = Data(environments, target)
+confidence_intervals(set([1,2]), np.array([0, 1, 1, 0, 0, 0]), data, 0.01)
