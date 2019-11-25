537a76ac1aa145c3db30e0a2a15f4080483bbff9
Juan Gamella <juangamella@gmail.com>
2019-11-25 13:19:48 +0100

    Started implementing policy classes + function to evaluate policies. Ongoing

--- src/main.py
+++ src/main.py
@@ -34,14 +34,54 @@ e4 = sem.sample(n, do_interventions = np.array([[4,5,1]]))
 print(sem.W)
 #_ = plot_graph(sem)
 
-environments = [e, e1, e4]#, e2, e3, e4
+environments = [e, e1]#, e2, e3, e4
 target = 3
 
-(S, accepted) = icp(environments, target, alpha=0.01, max_predictors=None, debug=True, stop_early=False)
-S,accepted
+(S, accepted,conf_intervals) = icp(environments, target, alpha=0.01, max_predictors=None, debug=True, stop_early=False)
 
-from icp import Data
-from icp import confidence_intervals
 
-data = Data(environments, target)
-confidence_intervals(set([1,2]), np.array([0, 1, 1, 0, 0, 0]), data, 0.01)
+
+class RandomPolicy():
+    def __init__(self, p, target):
+        self.p = p
+        self.target = target
+        self.vars = [i for i in range(p) if i != target]
+
+    def first(self, obs_data):
+        return np.random.choice(self.vars)
+    
+    def next(self, accepted, intervened):
+        not_intervened = [i for i in self.vars if i not in intervened]
+        if not not_intervened: # ie. not_intervened is empty
+            return None
+        else:
+            return np.random.choice(not_intervened)
+
+def test_policy(policy, sem, target, alpha = 0.01, n=round(1e5), n_i = round(1e5), max_iter = 100):
+    obs_data = sem.sample(n) # sample observational data
+    next_intervention = policy.first(obs_data) # get first intervention
+    i = 0
+    done = False
+    environments = [obs_data]
+    intervened = []
+    Accepted = []
+    while (i < max_iter) and next_intervention is not None:
+        print("\nIntervening on %d\n" % next_intervention)
+        e = sem.sample(n_i, do_interventions= np.array([[next_intervention, 10]]))
+        environments.append(e)
+        (S, accepted, conf_intervals) = icp(environments, target, alpha, max_predictors=None, debug=False, stop_early=False)
+        intervened.append(next_intervention)
+        next_intervention = policy.next(accepted, intervened)
+        Accepted.append(accepted)
+    return intervened, Accepted
+    
+    
+#test_policy(RandomPolicy(p, target), sem, target, max_iter=6)
+
+def ratios(p, accepted):
+    one_hot = np.zeros((len(accepted), p))
+    for i,s in enumerate(accepted):
+        one_hot[i, s] = 1
+    return one_hot.sum(axis=0) / len(accepted)
+        
+ratios(p, accepted)
