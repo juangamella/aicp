8da9d107cdfb7441a737e208bddfa327e80f8cbd
Juan Gamella <juangamella@gmail.com>
2020-04-04 21:56:32 +0200

    Added the ability to save/load test cases following ABCD format

--- src/run_experiments.py
+++ src/run_experiments.py
@@ -31,8 +31,21 @@
 import pickle
 import time
 from datetime import datetime
-from src import evaluation, policy, sampling
+from src import evaluation, policy, sampling, utils
 import argparse
+import os
+import numpy as np
+
+def parameter_string(args, excluded_keys):
+    """Convert a Namespace object (from argparse) into a string, excluding
+    some keys, to use as filename or dataset name"""
+    string = ""
+    for k,v in vars(args).items():
+        value = str(v)
+        value = value.replace('/', '')
+        if k not in excluded_keys:
+            string = string + "_" + k + ":" + value
+    return string
 
 # --------------------------------------------------------------------
 # Parse input parameters
@@ -58,9 +71,9 @@ arguments = {
     'max_iter': {'default': -1, 'type': int}, # -1
     'n': {'default': 100, 'type': int},
     'alpha': {'default': 0.01, 'type': float},
-    'tag' : {'default': "", 'type': str},
-    'save_dataset': {'default': "", 'type':str},
-    'load_dataset': {'default': "", 'type':str}}
+    'tag' : {'type': str},
+    'save_dataset': {'type': str},
+    'load_dataset': {'type': str}
 }
 
 
@@ -68,15 +81,16 @@ arguments = {
 parser = argparse.ArgumentParser(description='Run experiments')
 for name, params in arguments.items():
     if params['type']==bool:
-        action = {'action': 'store_true'}
+        options = {'action': 'store_true'}
     else:
-        action = {'action': 'store', 'type': params['type']}
+        options = {'action': 'store', 'type': params['type']}
+    if 'default' in params:
+        options['default'] = params['default']
     parser.add_argument("--" + name,
                         dest=name,
                         required=False,
-                        default=params['default'],
-                        **action)
-
+                        **options)
+excluded_keys = ['save_dataset', 'debug'] # Parameters that are excluded from the filename (see parameter_string function above)
 args = parser.parse_args()
 print(args)
 
@@ -90,21 +104,26 @@ else:
     
 P = args.n_min if args.n_min == args.n_max else (args.n_min, args.n_max)
 
+excluded_keys += ['tag'] if args.tag is None else []
+
 n_workers = None if args.n_workers == -1 else args.n_workers
 
-
+# Load dataset
 if args.load_dataset is not None:
+    print("\nLoading test cases from %s" % args.load_dataset)
     # Load a dataset stored in the format used by ABCD
     G = len(os.listdir(os.path.join(args.load_dataset, 'dags')))
     Ws = [np.loadtxt(os.path.join(args.load_dataset, 'dags', 'dag%d' % i, 'adjacency.txt')) for i in range(G)]
     means = [np.loadtxt(os.path.join(args.load_dataset, 'dags', 'dag%d' % i, 'means.txt')) for i in range(G)]
     variances = [np.loadtxt(os.path.join(args.load_dataset, 'dags', 'dag%d' % i, 'variances.txt')) for i in range(G)]
-    targets = [np.loadtxt(os.path.join(args.load_dataset, 'dags', 'dag%d' % i, 'target.txt')) for i in range(G)]
+    targets = [int(np.loadtxt(os.path.join(args.load_dataset, 'dags', 'dag%d' % i, 'target.txt'))) for i in range(G)]
     cases = []
     for i, W in enumerate(Ws):
         sem = sampling.LGSEM(W, variances[i], means[i])
-        truth = utils.graph_info(target[i], W)
-        cases.append(policy.TestCase(i, sem, target[i], truth))
+        truth = utils.graph_info(targets[i], W)
+        cases.append(policy.TestCase(i, sem, targets[i], truth))
+    excluded_keys += ['avg_deg', 'w_min', 'w_max', 'var_min', 'var_max', 'int_min', 'int_max', 'random_state', 'n_min', 'n_max']
+# Or generate dataset
 else:
     cases = evaluation.gen_cases(args.G,
                                  P,
@@ -116,6 +135,33 @@ else:
                                  args.int_min,
                                  args.int_max,
                                  args.random_state)
+    excluded_keys += ['load_dataset']
+
+# (Optionally) Save dataset according to format used by ABCD
+if args.save_dataset is not None and args.load_dataset is None:
+    # Compose directory name
+    exclude = ['n_workers',
+               'batch_size',
+               'debug',
+               'avg_deg',
+               'runs',
+               'finite',
+               'max_iter',
+               'n',
+               'alpha',
+               'save_dataset',
+               'load_dataset']
+    exclude += ['tag'] if args.tag is None else []
+    dir_name = args.save_dataset + "_%d" % time.time() + parameter_string(args, exclude)
+    print("\nSaving test cases under %s/" % dir_name)
+    # Save weighted adjacency matrix, means, variances and target
+    for i,case in enumerate(cases):
+        sem = case.sem
+        os.makedirs(os.path.join(dir_name, 'dags', 'dag%d' % i), exist_ok=True)
+        np.savetxt(os.path.join(dir_name, 'dags', 'dag%d' % i, 'adjacency.txt'), sem.W)
+        np.savetxt(os.path.join(dir_name, 'dags', 'dag%d' % i, 'means.txt'), sem.intercepts)
+        np.savetxt(os.path.join(dir_name, 'dags', 'dag%d' % i, 'variances.txt'), sem.variances)
+        np.savetxt(os.path.join(dir_name, 'dags', 'dag%d' % i, 'target.txt'), [case.target])
 
 # --------------------------------------------------------------------
 # Evaluate experiments
@@ -155,9 +201,10 @@ print("\n\nFinished experiments at %s (elapsed %0.2f seconds)" % (datetime.now()
 # --------------------------------------------------------------------
 # Save results
 
+# Compose filename
 filename = "experiments/results_%d" % end
-for k,v in vars(args).items():
-    filename = filename + "_" + k + ":" + str(v)
-filename = filename + ".pickle"
+filename = filename + parameter_string(args, excluded_keys) + ".pickle"
+
+# Pickle results
 pickle.dump([cases] + results, open(filename, "wb"))
 print("Saved to file \"%s\"" % filename)
