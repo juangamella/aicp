fb838c3d2f22760f8e8fad2f95e95660871a5e4e
Juan Gamella <juangamella@gmail.com>
2020-04-29 15:38:44 +0200

    Changed graph generation procedure: remove limitation that a markov blanket has to be a strict superset of parents

--- src/evaluation.py
+++ src/evaluation.py
@@ -57,7 +57,7 @@ def gen_cases(n, P, k, w_min=1, w_max=1, var_min=1, var_max=1, int_min=0, int_ma
         W = sempler.dag_avg_deg(p, k, w_min, w_max)
         target = np.random.choice(range(p))
         parents,_,_,mb = utils.graph_info(target, W)
-        if len(parents) > 0 and len(parents) != len(mb):
+        if len(parents) > 0:# and len(parents) != len(mb):
             sem = sempler.LGANM(W, (var_min, var_max), (int_min, int_max))
             (truth, _, _, _) = utils.graph_info(target, W)
             cases.append(TestCase(i, sem, target, truth))
