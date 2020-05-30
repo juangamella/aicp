ef701d284f2c32b88be291e1ede5c32aa6f55f73
Juan Gamella <juangamella@gmail.com>
2020-05-30 17:16:51 +0200

    Anonymized license files

--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Juan L Gamella
+# Copyright 2019 XXX
 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
